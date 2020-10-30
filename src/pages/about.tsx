8d82497ad05b5078cf9063d639d308b9dc57e314
Josiah Tan <55021839+jolistic@users.noreply.github.com>
2020-10-30 07:00:35 +0200

    Change gdrive link to preview instead of edit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,7 +16,7 @@ const About = () => {
       <SEO title="About | Josiah" desc="Hi. I'm Josiah! Here's how to contact me." />
       <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
         <h1>Hi. I'm Josiah!</h1>
-        <iframe src="https://drive.google.com/file/d/1tspvykfLtDW9-Di31XAYjCd2RBzWfTXi/view?usp=sharing" width="400px" height="300px"  />
+        <iframe src="https://drive.google.com/file/d/1tspvykfLtDW9-Di31XAYjCd2RBzWfTXi/preview?usp=drivesdk" width="400px" height="300px"  />
         
         <p>Here's how to contact me:</p>
         <li>
